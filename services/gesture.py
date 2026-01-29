76dd8a8664138d536aac73b2ffe797df6ecec412
Hariom Hatwate <hariomhatvate@gmail.com>
2026-01-29 18:18:47 +0530

    Force Python 3.11 using Docker (fix mediapipe)

--- services/gesture.py
+++ services/gesture.py
@@ -2,26 +2,16 @@ import cv2
 import mediapipe as mp
 
 mp_hands = mp.solutions.hands
-mp_draw = mp.solutions.drawing_utils
-
 hands = mp_hands.Hands(
     static_image_mode=False,
-    max_num_hands=1,
-    min_detection_confidence=0.7,
-    min_tracking_confidence=0.7
+    max_num_hands=2,
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5
 )
 
 def detect_sign(frame):
-    image_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    results = hands.process(image_rgb)
-
+    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    results = hands.process(rgb)
     if results.multi_hand_landmarks:
-        for hand_landmarks in results.multi_hand_landmarks:
-            mp_draw.draw_landmarks(
-                frame,
-                hand_landmarks,
-                mp_hands.HAND_CONNECTIONS
-            )
         return "HAND DETECTED"
-
     return None
